07f4d380760af44d109fab4214d73677bb066d39
jcaspian <jeff_caspian@hotmail.com>
2014-01-13 15:50:00 +0800

    Add useful stuff

--- Projects/app1/platforms/android/gen/R.java.d
+++ Projects/app1/platforms/android/gen/R.java.d
@@ -1,4 +1,4 @@
-D:\Github\android-webapp\Projects\app1\platforms\android\gen\com\jcaspian\easy_bubbleo2\R.java \
+D:\Github\android-webapp\Projects\app1\platforms\android\gen\com\jcaspian\easy_bubbleo2_1\R.java \
  : D:\Github\android-webapp\Projects\app1\platforms\android\res\drawable\icon.png \
 D:\Github\android-webapp\Projects\app1\platforms\android\res\drawable-hdpi\icon.png \
 D:\Github\android-webapp\Projects\app1\platforms\android\res\drawable-ldpi\icon.png \
